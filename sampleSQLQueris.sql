eac57a2b8bb4dc653c6f752935f00ec6b68c1176
agroenewold <groenewold.andrew@gmail.com>
2015-12-15 22:08:22 -0500

    Update sampleSQLQueris.sql

--- sampleSQLQueris.sql
+++ sampleSQLQueris.sql
@@ -1,12 +1,22 @@
---Sample Queries for the android app
+--
+-- sampleSQLQueris.sql contains several sample queries to demonstrate
+-- how to use the kimSQL database.
+--
+-- @author Andrew Groenewold and Moses Mangunrahardja
+-- @version fall, 2015
+--
+--Select everything in the database
 --Select * from Account;
 --Select * from Stock;
 
---Select ticker from Stock where weekPercentChange < 0;
+
+--Choose only the expensive stocks
+--Select ticker from Stock where buyPrice > 50;
+
+--Choose all the credentials with admin rights
 --Select userID, password from Account where isAdmin = TRUE;
 
---Select ticker,  (currentPrice - buyPrice)*sharesOwned as totalDollarChange
---from Stock;
 
-Select ticker, password
+--Select all the stocks from the same sector as the account with 'boopower' as its password
+Select ticker
 from Account, Stock where Account.userID = Stock.sector AND password = 'boopower';
